a16ae6b5bb5dc74bccbd91fef8a586e83d14c52b
Ibrahim <ibrahimzerlin@hotmail.com>
2021-05-13 16:35:06 -0500

    added post new posts and comments

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const createPostRoutes = require('./create-post-routes.js')
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/create-post', createPostRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
